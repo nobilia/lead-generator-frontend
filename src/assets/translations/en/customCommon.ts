8cb0e313db92f97243f12909db452957af71f465
Andrei Patsihau <a.patsihau@sam-solutions.com>
2023-01-11 21:15:22 +0300

    LG-584 - [LG-US-105][LG] Change password [FE]

--- src/assets/translations/en/customCommon.ts
+++ src/assets/translations/en/customCommon.ts
@@ -1,9 +1,12 @@
 export const customCommon = {
   httpHandlers: {
     badRequestPleaseLoginAgain: 'Your username or password was incorrect.',
+    badRequestOldPasswordIncorrect: 'Please enter your current password'
   },
   formErrors: {
-    required: 'This is a mandatory field',
-    cxInvalidEmail: 'Invalid e-mail address'
+    required: 'This field is required',
+    cxInvalidEmail: 'Invalid e-mail address',
+    cxInvalidPassword: 'This is not a valid password format',
+    cxPasswordsMustMatch: 'Password fields must match'
   }
 };
--- src/assets/translations/en/index.ts
+++ src/assets/translations/en/index.ts
@@ -2,10 +2,12 @@ import {customCommon} from './customCommon';
 import {dashboardLogin} from './dashboard-login.i18n';
 import {dashboardMenu} from './dashboard-menu.i18n';
 import {dashboardLeads} from "./dashboard-leads.i18n";
+import {dashboardUpdatePassword} from "./dashboard-change-password.i18n";
 
 export const en = {
   customCommon,
   dashboardLogin,
   dashboardMenu,
-  dashboardLeads
+  dashboardLeads,
+  dashboardUpdatePassword
 };
